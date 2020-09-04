802b511773885dbc3cec32ed922afda01b726205
Leonardo Silva <leonardo.silva1303@etec.sp.gov.br>
2020-09-04 19:52:44 -0300

    Splash Screen Concluída
    Inserção de uma splash screen animada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,16 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { timer } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  showSplash = true;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
@@ -22,6 +26,9 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      timer(3000).subscribe(() => this.showSplash = false)
+
     });
   }
 }
