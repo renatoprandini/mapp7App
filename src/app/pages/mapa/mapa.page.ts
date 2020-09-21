89fcc464d81bdc047ba35f1ce0d465cfc5731293
Leonardo Silva <leonardo.silva1303@etec.sp.gov.br>
2020-09-21 17:16:09 -0300

    Update mapa.page.ts

--- src/app/pages/mapa/mapa.page.ts
+++ src/app/pages/mapa/mapa.page.ts
@@ -1,5 +1,5 @@
 import { Component, AfterViewInit } from '@angular/core';
-import { Post } from 'src/app/models/post.model';
+
 
 @Component({
   selector: 'app-mapa',
@@ -7,15 +7,14 @@ import { Post } from 'src/app/models/post.model';
   styleUrls: ['./mapa.page.scss'],
 })
 export class MapaPage implements AfterViewInit {
-  public post: Post = new Post('', '', null);
 
   constructor() {
-    const data = localStorage.getItem('baltagram.post');
-    if (data) { this.post = JSON.parse(data); }
-  }
 
-  ngAfterViewInit() {
-   
+
+
+
+}  ngAfterViewInit(): void {
+    throw new Error('Method not implemented.');
   }
 
 }
